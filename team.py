fdc5826963b488f3897ef93d3e63d14f34a288a5
cxlanyagege <cxlanyagege@hotmail.com>
2024-03-25 23:54:02 +1100

    Construct basic workspace

---
+++ team.py
@@ -0,0 +1,29 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Partner Nomination
+
+# Fill in your team details here. Be sure to keep a copy of this file for use in
+# future project submissions (this is how we identify your team).
+
+metadata = {
+    # Choose a team name! Try to make it unique in some way, as it will be
+    # used to identify your team in the tournament at the end of the semester.
+    # - Allowed characters: A-Z, a-z, 0-9, -, _ (no spaces allowed)
+    # - Minimum length: 3 characters
+    # - Maximum length: 20 characters
+    "team_name": f"AIH1", 
+
+    # Fill in the student IDs and emails of both team members here. There must
+    # be exactly two members in the list. 
+    "team_members": [
+        {
+            "name": "Ruolan Su",
+            "student_id": "1297549",
+            "email": "lanruos@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        },
+        {
+            "name": "He Shen",
+            "student_id": "1297447",
+            "email": "heshen@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        }
+    ],
+}
